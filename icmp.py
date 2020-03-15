e32356abd7243700a0ca55e0db9d202239a33b1e
Jeff Hoyopatubbi <jeff.hoyopatubbi@gmail.com>
2020-03-14 21:39:40 -0700

    Changed scapy projects.

--- icmp.py
+++ icmp.py
@@ -20,13 +20,21 @@ def packet_callback(packet):
                 print("[%s] ICMP Echo Reply %s -> %s" % (packet.getlayer(ICMP).type,packet[IP].src, packet[IP].dst))
             elif icmp_type == "8":
                 print("[%s] ICMP ECHO Request %s -> %s" % (packet.getlayer(ICMP).type,packet[IP].src, packet[IP].dst))
+            elif icmp_type == "3":
+                print("[%s] ICMP Traceroute Packet %s -> %s" % (packet.getlayer(ICMP).type,packet[IP].src, packet[IP].dst))
             else:
                 print("[%s] ICMP Type not ECHO" % icmp_type)
                 #print("ICMP Not Specified Type: %s" % icmp_type)
         elif ip_type == "6":
             print("[%s] TCP Packet Detected" % packet.getlayer(IP).proto)
         elif ip_type == "17":
-            print("[%s] UDP Packet Detected" % packet.getlayer(IP).proto)
+
+            udp_ttl = str(packet.getlayer(IP).ttl)
+
+            if udp_ttl == "1":
+                print("[TTL: %s] UDP Traceroute Paket Detected" % udp_ttl)
+            else:
+                print("[%s] UDP Packet Detected" % packet.getlayer(IP).proto)
         else:
             print("[%s] Unknown Protocol Detected" % packet.getlayer(IP).proto)
     elif icmp_osx == "30":
--- single_packet.py
+++ single_packet.py
@@ -3,11 +3,14 @@ from scapy.all import *
 # our packet callback
 def packet_callback(packet):
 
-    # print(packet.summary())
-    # print("-----------------")
-    print(packet.getlayer(Dot3).dst)
-    print(packet.getlayer(Dot3).src)
-    print(packet.getlayer(Dot3).len)
+    print(packet.summary())
+    print("-----------------")
+    print(packet.show())
+    print("-----------------")
+    print(packet.getlayer(IP in ICMP).ttl)
+    # print(packet.getlayer(Dot3).dst)
+    # print(packet.getlayer(Dot3).src)
+    # print(packet.getlayer(Dot3).len)
 
 # fire up our sniffer
-sniff(iface="en0",filter="stp",prn=packet_callback,count=50)
+sniff(iface="lo0",prn=packet_callback,count=50)
